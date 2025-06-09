67fc1352796d2a229762335e273e9a4b482af1a8
Yoshida672 <ericissamuyoshida@gmail.com>
2025-06-09 00:40:42 -0300

    Add detalhes

--- src/main/java/br/com/fiap/globalsolution2025/mapper/AlertaMapper.java
+++ src/main/java/br/com/fiap/globalsolution2025/mapper/AlertaMapper.java
@@ -2,6 +2,7 @@ package br.com.fiap.globalsolution2025.mapper;
 
 import br.com.fiap.globalsolution2025.controller.DadosController;
 import br.com.fiap.globalsolution2025.dto.request.AlertaRequest;
+import br.com.fiap.globalsolution2025.dto.request.UpdateAlertaRequest;
 import br.com.fiap.globalsolution2025.dto.response.AlertaResponse;
 import br.com.fiap.globalsolution2025.entity.Alerta;
 import org.springframework.hateoas.Link;
@@ -13,12 +14,26 @@ import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 @Component
 public class AlertaMapper {
 
-    public Alerta toEntity(AlertaRequest request) {
-        Alerta alerta = new Alerta();
+    public Alerta toEntity(AlertaRequest request,Alerta alerta) {
         alerta.setTipo(request.tipo());
         alerta.setMensagem(request.mensagem());
         alerta.setNivelRisco(request.nivelRisco());
         alerta.setStatus(request.status());
+        return alerta;
+    }
+    public Alerta toEntity(UpdateAlertaRequest request, Alerta alerta) {
+        if (request.tipo() != null) {
+            alerta.setTipo(request.tipo());
+        }
+        if (request.mensagem() != null) {
+            alerta.setMensagem(request.mensagem());
+        }
+        if (request.nivelRisco() != null) {
+            alerta.setNivelRisco(request.nivelRisco());
+        }
+        if (request.status() != null) {
+            alerta.setStatus(request.status());
+        }
         return alerta;
     }
 
--- src/main/java/br/com/fiap/globalsolution2025/service/AlertaService.java
+++ src/main/java/br/com/fiap/globalsolution2025/service/AlertaService.java
@@ -1,6 +1,7 @@
 package br.com.fiap.globalsolution2025.service;
 
 import br.com.fiap.globalsolution2025.dto.request.AlertaRequest;
+import br.com.fiap.globalsolution2025.dto.request.UpdateAlertaRequest;
 import br.com.fiap.globalsolution2025.entity.Alerta;
 import br.com.fiap.globalsolution2025.mapper.AlertaMapper;
 import br.com.fiap.globalsolution2025.repository.AlertaRepository;
@@ -8,7 +9,13 @@ import jakarta.persistence.EntityNotFoundException;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
+
+import static org.springframework.util.ClassUtils.isPresent;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.CachePut;
 
 @Service
 public class AlertaService {
@@ -21,30 +28,32 @@ public class AlertaService {
         this.mapper = mapper;
     }
 
+    @CachePut(value = "alertas", key = "#result.id")
     public Alerta save(AlertaRequest request) {
-        Alerta alerta = mapper.toEntity(request);
+        Alerta alerta = mapper.toEntity(request, new Alerta());
         return repository.save(alerta);
     }
 
+    @Cacheable(value = "alertas", key = "#id")
     public Alerta getById(Long id) {
         return repository.findById(id)
                 .orElseThrow(() -> new EntityNotFoundException("Alerta não encontrado: " + id));
     }
 
+    @Cacheable(value = "alertasTodos")
     public List<Alerta> getAll() {
         return repository.findAll();
     }
 
-    public Alerta update(Long id, AlertaRequest request) {
-        if(repository.findById(id).isPresent()){
-            Alerta alerta = mapper.toEntity(request);
-            return repository.save(alerta);
-        }
-        else{
-            return null;
-        }
+    @CachePut(value = "alertas", key = "#id")
+    public Alerta update(Long id, UpdateAlertaRequest request) throws Exception {
+        Alerta alerta = repository.findById(id)
+                .orElseThrow(() -> new Exception("Alerta não encontrado"));
+        mapper.toEntity(request, alerta);
+        return repository.save(alerta);
     }
 
+    @CacheEvict(value = {"alertas", "alertasTodos"}, key = "#id")
     public void delete(Long id) {
         if (!repository.existsById(id)) {
             throw new EntityNotFoundException("Alerta não encontrado: " + id);
